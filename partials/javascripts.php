c3cff42110e16cda798cf057ee8df7187d3cc93c
shapack <azeem3z123@gmail.com>
2019-05-17 00:38:36 +0500

    cleaned the js libraries

--- partials/javascripts.php
+++ partials/javascripts.php
@@ -1,15 +1,15 @@
 <!-- jQuery -->
-<script src="assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="assets/js-libs/jquery/jquery.min.js"></script>
 <script src="dist/js/jquery.ui.touch-punch-improved.js"></script>
 <script src="dist/js/jquery-ui.min.js"></script>
 
 <!-- Bootstrap tether Core JavaScript -->
-<script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-<script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/js-libs/popper/popper.min.js"></script>
+<script src="assets/js-libs/bootstrap/bootstrap.min.js"></script>
 
 <!-- slimscrollbar scrollbar JavaScript -->
-<script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-<script src="assets/extra-libs/sparkline/sparkline.js"></script>
+<script src="assets/js-libs/scrollbar/perfect-scrollbar.jquery.min.js"></script>
+<script src="assets/js-libs/sparkline/sparkline.js"></script>
 
 <!--Wave Effects -->
 <script src="dist/js/waves.js"></script>
@@ -18,51 +18,51 @@
 <script src="dist/js/sidebarmenu.js"></script>
 
 <!-- index2 page js -->
-<script src="assets/libs/moment/min/moment.min.js"></script>
-<script src="assets/libs/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="assets/js-libs/moment/moment.min.js"></script>
+<script src="assets/js-libs/calendar/fullcalendar.min.js"></script>
 <script src="dist/js/pages/calendar/cal-init.js"></script>
 
 <!-- forms page js -->
-<script src="assets/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
+<script src="assets/js-libs/inputmask/jquery.inputmask.bundle.min.js"></script>
 <script src="dist/js/pages/mask/mask.init.js"></script>
-<script src="assets/libs/select2/dist/js/select2.full.min.js"></script>
-<script src="assets/libs/select2/dist/js/select2.min.js"></script>
-<script src="assets/libs/jquery-asColor/dist/jquery-asColor.min.js"></script>
-<script src="assets/libs/jquery-asGradient/dist/jquery-asGradient.js"></script>
-<script src="assets/libs/jquery-asColorPicker/dist/jquery-asColorPicker.min.js"></script>
-<script src="assets/libs/jquery-minicolors/jquery.minicolors.min.js"></script>
-<script src="assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<script src="assets/libs/quill/dist/quill.min.js"></script>
-<script src="assets/libs/jquery-steps/build/jquery.steps.min.js"></script>
-<script src="assets/libs/jquery-validation/dist/jquery.validate.min.js"></script>
+<script src="assets/js-libs/select2/select2.full.min.js"></script>
+<script src="assets/js-libs/select2/select2.min.js"></script>
+<script src="assets/js-libs/jquery-asColor/jquery-asColor.min.js"></script>
+<script src="assets/js-libs/jquery-asGradient/jquery-asGradient.js"></script>
+<script src="assets/js-libs/jquery-asColorPicker/jquery-asColorPicker.min.js"></script>
+<script src="assets/js-libs/jquery-minicolors/jquery.minicolors.min.js"></script>
+<script src="assets/js-libs/datepicker/bootstrap-datepicker.min.js"></script>
+<script src="assets/js-libs/quill/quill.min.js"></script>
+<script src="assets/js-libs/jquery-steps/jquery.steps.min.js"></script>
+<script src="assets/js-libs/jquery-validation/jquery.validate.min.js"></script>
 
 <!-- pages-gallery page js -->
-<script src="assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-<script src="assets/libs/magnific-popup/meg.init.js"></script>
+<script src="assets/js-libs/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="assets/js-libs/magnific-popup/meg.init.js"></script>
 
 <!-- pages-elements page js -->
-<script src="assets/libs/toastr/build/toastr.min.js"></script>
+<script src="assets/js-libs/toastr/toastr.min.js"></script>
 
 <!-- tables page js -->
-<script src="assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
-<script src="assets/extra-libs/multicheck/jquery.multicheck.js"></script>
-<script src="assets/extra-libs/DataTables/datatables.min.js"></script>
+<script src="assets/js-libs/multicheck/datatable-checkbox-init.js"></script>
+<script src="assets/js-libs/multicheck/jquery.multicheck.js"></script>
+<script src="assets/js-libs/datatables/datatables.min.js"></script>
 
 <!--Custom JavaScript -->
 <script src="dist/js/custom.min.js"></script>
 
 <!-- Charts js Files -->
-<script src="assets/libs/flot/excanvas.js"></script>
-<script src="assets/libs/flot/jquery.flot.js"></script>
-<script src="assets/libs/flot/jquery.flot.pie.js"></script>
-<script src="assets/libs/flot/jquery.flot.time.js"></script>
-<script src="assets/libs/flot/jquery.flot.stack.js"></script>
-<script src="assets/libs/flot/jquery.flot.crosshair.js"></script>
-<script src="assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+<script src="assets/js-libs/flot/excanvas.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.pie.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.time.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.stack.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.crosshair.js"></script>
+<script src="assets/js-libs/flot/jquery.flot.tooltip.min.js"></script>
 <script src="dist/js/pages/chart/chart-page-init.js"></script>
-<script src="assets/libs/chart/matrix.interface.js"></script>
-<script src="assets/libs/chart/excanvas.min.js"></script>
-<script src="assets/libs/chart/jquery.peity.min.js"></script>
-<script src="assets/libs/chart/matrix.charts.js"></script>
-<script src="assets/libs/chart/jquery.flot.pie.min.js"></script>
-<script src="assets/libs/chart/turning-series.js"></script>
+<script src="assets/js-libs/chart/matrix.interface.js"></script>
+<script src="assets/js-libs/chart/excanvas.min.js"></script>
+<script src="assets/js-libs/chart/jquery.peity.min.js"></script>
+<script src="assets/js-libs/chart/matrix.charts.js"></script>
+<script src="assets/js-libs/chart/jquery.flot.pie.min.js"></script>
+<script src="assets/js-libs/chart/turning-series.js"></script>
